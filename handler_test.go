ca965e11dfbc095fe8f8e5657668a33f937e91ef
husada20 <husadahutasoit@gmail.com>
2023-08-08 19:18:15 +0700

    go web

---
+++ handler_test.go
@@ -0,0 +1,50 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		// logic web
+		fmt.Fprintf(writer, "Hello batman")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// ServeMux / router
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello batman")
+	})
+
+	mux.HandleFunc("/images", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "images")
+	})
+
+	mux.HandleFunc("/images/thumbnails", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "thumbnails")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
