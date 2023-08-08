36ed3ac444add7a96a1b1d53d8e2127303db6de8
husada20 <husadahutasoit@gmail.com>
2023-08-08 19:48:23 +0700

    request

--- handler_test.go
+++ handler_test.go
@@ -48,3 +48,21 @@ func TestServeMux(t *testing.T) {
 		panic(err)
 	}
 }
+
+// request
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, request.Method)
+		fmt.Fprint(writer, request.RequestURI)
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
