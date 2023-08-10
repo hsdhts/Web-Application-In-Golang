b7c160f2e6d5636718738b9fb5213be3c460a250
husada20 <husadahutasoit@gmail.com>
2023-08-10 23:32:49 +0700

    query param

---
+++ query_param_test.go
@@ -0,0 +1,32 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SayHello(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+	if name == "" {
+		fmt.Fprintf(writer, "hello")
+	} else {
+		fmt.Fprint(writer, "hello %s ", name)
+	}
+}
+
+func TestQueryParameter(t *testing.T) {
+
+	request := httptest.NewRequest("GET", "httpp:localhost:8080/hello?name=batman", nil)
+	recorder := httptest.NewRecorder()
+
+	SayHello(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+
+}
