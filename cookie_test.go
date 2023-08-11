88cdb28b3b45d4fc8b0ebc5c966c0f0279603a89
husada20 <husadahutasoit@gmail.com>
2023-08-12 00:29:23 +0700

    add cookie

--- cookie_test.go
+++ cookie_test.go
@@ -2,7 +2,10 @@ package golang_web
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func SetCookie(writer http.ResponseWriter, request *http.Request) {
@@ -19,6 +22,54 @@ func SetCookie(writer http.ResponseWriter, request *http.Request) {
 func GetCookie(writer http.ResponseWriter, request *http.Request) {
 	cookie, err := request.Cookie("X-Batman-name")
 	if err != nil {
-		fmt.Fprint(writer, "No cookie")
+		fmt.Fprintf(writer, "No cookie")
+	} else {
+		name := cookie.Value
+		fmt.Fprintf(writer, "Hello %s", name)
 	}
+}
+
+// test cookie with mux
+
+func TestCookie(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/set-cookie", SetCookie)
+	mux.HandleFunc("/get-cookie", GetCookie)
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSetCookie(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SetCookie(recorder, request)
+
+	cookies := recorder.Result().Cookies()
+	for _, cookie := range cookies {
+		fmt.Printf("Cookie %s:%s \n", cookie.Name, cookie.Value)
+	}
+}
+
+func TestGetCookie(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	cookie := new(http.Cookie)
+	cookie.Name = "x-Batman-Name"
+	cookie.Value = "Superman"
+	request.AddCookie(cookie)
+
+	recorder := httptest.NewRecorder()
+
+	GetCookie(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
 }
