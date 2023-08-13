83d21df95a1265aa3fd51e4eff7130dcabcb21c1
husada20 <husadahutasoit@gmail.com>
2023-08-13 22:41:11 +0700

    file server

--- file_server_test.go
+++ file_server_test.go
@@ -1,6 +1,7 @@
 package golang_web
 
 import (
+	"embed"
 	"net/http"
 	"testing"
 )
@@ -8,6 +9,26 @@ import (
 func TestFileServer(t *testing.T) {
 	directory := http.Dir("./resources")
 	fileServer := http.FileServer(directory)
+
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources
+var resources embed.FS
+
+func TestFileServerEmbed(t *testing.T) {
+	fileServer := http.FileServer(http.FS(resources))
 
 	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
