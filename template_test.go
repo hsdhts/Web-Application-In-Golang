acead627c32af9c5ed0de653ae21b22ad2e00a20
husada20 <husadahutasoit@gmail.com>
2023-08-14 23:27:06 +0700

    template

--- template_test.go
+++ template_test.go
@@ -1,8 +1,12 @@
 package golang_web
 
 import (
+	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func TemplateText(writer http.ResponseWriter, request *http.Request) {
@@ -14,4 +18,14 @@ func TemplateText(writer http.ResponseWriter, request *http.Request) {
 
 	t := template.Must(template.New("SIMPLE").Parse(templateTest))
 	t.ExecuteTemplate(writer, "SIMPLE", "Hello HTML Template")
+}
+
+func TestSimpleHTML(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateText(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
 }
